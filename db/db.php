40a013c36a00debceeb356aa990c965e1cefb3ec
nagy- <hybase@outlook.hu>
2023-04-20 13:29:01 +0200

    database connection new file and class.

---
+++ db/db.php
@@ -0,0 +1,53 @@
+<?php
+
+class Db {
+	private static $server_name = "localhost";
+	private static $user_name = "root";
+	private static $password = "";
+	private static $db_name = "gamf_car";
+	private static $connection = null;
+
+    /*
+
+    *** Egy fontos osztály, az itt szereplő kapcsolattal létre hoz egy adatbázis kapcsolatot (ha nem létezik már)
+    *** és a statikus GetInstance() függvénye vissza adja ezt. Így biztosak lehetünk benne, hogy mindig csak egy
+    *** adatbázis kapcsolatunk létezik a kód során.
+    
+
+
+	*** Tovább fejlesztésnél néhány SQL parancsot elkészíthetünk előre, ahol meg tudjuk adni a tábla
+    *** nevét és a paramétereit (WHERE id = $id)
+
+    *** Ennél a feladatnál a GetInstance() függvény elegendő
+
+
+    public static function Query($sql,$params = []) {
+        if (!self::$connection) {
+            self::$connection = mysqli_connect(self::$servername,self::$username,self::$password);
+            
+        }
+        return self::$connection;
+    }
+
+
+    public static function Select($table, $where=null) {
+        $sql = "SELECT * FROM ".$table;
+        if ($where) {$sql .= (" WHERE " . $where);}
+        $query = self::Query($sql);
+        return $query ->fetchAll();
+    }
+    */
+
+    public static function GetInstance(){
+        if (!self::$connection) {
+            
+            self::$connection = mysqli_connect(self::$server_name,self::$user_name,self::$password,self::$db_name);
+            
+        }
+
+        return self::$connection;
+    }
+
+}
+
+?>
